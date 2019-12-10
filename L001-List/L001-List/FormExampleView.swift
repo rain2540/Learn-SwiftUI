955d03f2d3e412b0822be31c23cb09f95c99939f
Smartech <rain2540@foxmail.com>
2019-12-10 15:40:27 +0800

    feat (L001 - FormExampleView) : add FormExampleView

---
+++ L001-List/L001-List/FormExampleView.swift
@@ -0,0 +1,21 @@
+//
+//  FormExampleView.swift
+//  L001-List
+//
+//  Created by RAIN on 2019/12/10.
+//  Copyright © 2019 Smartech. All rights reserved.
+//
+
+import SwiftUI
+
+struct FormExampleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FormExampleView_Previews: PreviewProvider {
+    static var previews: some View {
+        FormExampleView()
+    }
+}
