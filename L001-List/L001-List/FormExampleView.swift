cc3f48bcc1c5bbe0b1fd2bec0250421ef0737bc3
Smartech <rain2540@foxmail.com>
2019-12-10 16:32:54 +0800

    style (L001 - FormExampleView) : update style

--- L001-List/L001-List/FormExampleView.swift
+++ L001-List/L001-List/FormExampleView.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct FormExampleView: View {
   
   let options = ["One", "Two"]
-  
+
   @State private var text = ""
   @State private var password = ""
   @State private var toggleOn = true
@@ -19,52 +19,52 @@ struct FormExampleView: View {
   @State private var date = Date()
   @State private var sliderValue = 4.0
   @State private var stepperValue = 1
-  
+
   var body: some View {
     NavigationView {
       Form {
-        
+
         Section(header: Text("First Section")) {
-          
+
           Text("Text")
-          
+
           Image(systemName: "circle")
-          
+
           TextField("TextField", text: $text)
-          
+
           SecureField("SecureField", text: $password)
         }
-        
+
         Section(header: Text("Second Section")) {
-          
+
           Button("Button") {
             print("CLick Button in a list row")
           }
-          
+
           Toggle("Toggle", isOn: $toggleOn)
-          
+
           HStack {
             Text("Slider, Value: \(Int(sliderValue))")
             Slider(value: $sliderValue, in: 0 ... 10)
           }
-          
+
           Stepper("Stepper, Value: \(stepperValue)", value: $stepperValue)
         }
-        
+
         Section(header: Text("Third Section")) {
           Picker(selection: $pickerSelection, label: Text("Picker")) {
             ForEach(0 ..< options.count) {
               Text(self.options[$0]).tag($0)
             }
           }
-          
+
           DatePicker("DatePicker", selection: $date)
         }
       }
       .navigationBarTitle(Text("Form Examples"))
     }
   }
-  
+
 }
 
 struct FormExampleView_Previews: PreviewProvider {
