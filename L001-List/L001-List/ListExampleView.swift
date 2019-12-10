deb0bb54de111789ac399b71176d8357d2620948
Smartech <rain2540@foxmail.com>
2019-12-10 15:42:26 +0800

    feat (L001 - ListExampleView) : add List

--- L001-List/L001-List/ListExampleView.swift
+++ L001-List/L001-List/ListExampleView.swift
@@ -9,9 +9,13 @@
 import SwiftUI
 
 struct ListExampleView: View {
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Text("Text")
+        }
     }
+
 }
 
 struct ListExampleView_Previews: PreviewProvider {
