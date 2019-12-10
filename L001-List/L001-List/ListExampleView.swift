70fe2094c05e93048929b605f2124794fb804acf
Smartech <rain2540@foxmail.com>
2019-12-10 15:39:43 +0800

    feat (L001 - ListExampleView) : add ListExampleView

---
+++ L001-List/L001-List/ListExampleView.swift
@@ -0,0 +1,21 @@
+//
+//  ListExampleView.swift
+//  L001-List
+//
+//  Created by RAIN on 2019/12/10.
+//  Copyright © 2019 Smartech. All rights reserved.
+//
+
+import SwiftUI
+
+struct ListExampleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListExampleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListExampleView()
+    }
+}
