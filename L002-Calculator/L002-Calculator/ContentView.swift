dce56e225821a4fed5b864fd246961bc09a07858
Smartech <rain2540@foxmail.com>
2019-12-10 16:45:31 +0800

    feat (L002) : add L002

---
+++ L002-Calculator/L002-Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  L002-Calculator
+//
+//  Created by RAIN on 2019/12/10.
+//  Copyright © 2019 Smartech. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ L002-Calculator/L002-CalculatorTests/L002_CalculatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  L002_CalculatorTests.swift
+//  L002-CalculatorTests
+//
+//  Created by RAIN on 2019/12/10.
+//  Copyright © 2019 Smartech. All rights reserved.
+//
+
+import XCTest
+@testable import L002_Calculator
+
+class L002_CalculatorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
