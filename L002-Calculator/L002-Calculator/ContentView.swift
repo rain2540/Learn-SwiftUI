ab14fb5363d2a7bb956993ccacb3eed25599ddfe
Smartech <rain2540@foxmail.com>
2020-05-19 16:11:06 +0800

    refactor (L002 - ContentView) : adjust UI build

--- L002-Calculator/L002-Calculator/ContentView.swift
+++ L002-Calculator/L002-Calculator/ContentView.swift
@@ -7,10 +7,11 @@
 //
 
 import SwiftUI
+import Combine
+
+let scale: CGFloat = UIScreen.main.bounds.width / 414
 
 struct ContentView: View {
-
-  let scale: CGFloat = UIScreen.main.bounds.width / 414
 
   var body: some View {
     VStack(spacing: 12) {
@@ -18,13 +19,12 @@ struct ContentView: View {
       Text("0")
         .font(.system(size: 76))
         .minimumScaleFactor(0.5)
-        .padding(.trailing, 24)
+        .padding(.trailing, 24 * scale)
         .lineLimit(1)
         .frame(
           minWidth: 0,
           maxWidth: .infinity,
           alignment: .trailing)
-        .scaleEffect(scale)
       CalculatorButtonPad()
         .padding(.bottom)
     }
@@ -64,7 +64,9 @@ struct CalculatorButtonRow: View {
         CalculatorButton(
           title: item.title,
           size: item.size,
-          backgroundColorName: item.backgroundColorName) {
+          backgroundColorName: item.backgroundColorName,
+          foregroundColor: item.foregroundColor)
+        {
             print("Button: \(item.title)")
         }
       }
@@ -80,16 +82,17 @@ struct CalculatorButton: View {
   let title: String
   let size: CGSize
   let backgroundColorName: String
+  let foregroundColor: Color
   let action: () -> Void
 
   var body: some View {
     Button(action: action) {
       Text(title)
-        .font(.system(size: fontSize))
-        .foregroundColor(.white)
-        .frame(width: size.width, height: size.height)
+        .font(.system(size: fontSize * scale))
+        .foregroundColor(foregroundColor)
+        .frame(width: size.width * scale, height: size.height * scale)
         .background(Color(backgroundColorName))
-        .cornerRadius(size.width / 2)
+        .cornerRadius(size.width * scale / 2)
     }
   }
 }
