a216e2eb5dd582bd136e553d620ca444c5d1ff8c
Smartech <rain2540@foxmail.com>
2020-06-09 14:57:35 +0800

    feat (L002 - CalculatorModel) : add method 'keepHistory'

--- L002-Calculator/L002-Calculator/CalculatorModel.swift
+++ L002-Calculator/L002-Calculator/CalculatorModel.swift
@@ -25,11 +25,28 @@ class CalculatorModel: ObservableObject {
         history.count + temporaryKept.count
     }
 
-    var slidingIndex: Float = 0
+    var slidingIndex: Float = 0 {
+        didSet {
+            keepHistory(upTo: Int(slidingIndex))
+        }
+    }
 
     func apply(_ item: CalculatorButtonItem) {
         brain = brain.apply(item: item)
         history.append(item)
+    }
+
+    func keepHistory(upTo index: Int) {
+        precondition(index <= totalCount, "Out of index.")
+
+        let total = history + temporaryKept
+
+        history = Array(total[..<index])
+        temporaryKept = Array(total[..<index])
+
+        brain = history.reduce(CalculatorBrain.left("0"), { (result, item) in
+            result.apply(item: item)
+        })
     }
 
 }
