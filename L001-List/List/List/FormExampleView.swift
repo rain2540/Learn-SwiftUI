4402bbf8d11c22ba3e2adac55b505a89a03ceeb4
Rain2540 <rain2540@foxmail.com>
2020-07-08 00:27:49 +0800

    chore (L001 - project) : add FormExampleView

---
+++ L001-List/List/List/FormExampleView.swift
@@ -0,0 +1,20 @@
+//
+//  FormExampleView.swift
+//  List
+//
+//  Created by Rain on 2020/7/8.
+//
+
+import SwiftUI
+
+struct FormExampleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FormExampleView_Previews: PreviewProvider {
+    static var previews: some View {
+        FormExampleView()
+    }
+}
