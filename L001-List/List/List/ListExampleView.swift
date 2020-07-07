a4dfae9958719da3f8a3faee515613e3fac85fa3
Rain2540 <rain2540@foxmail.com>
2020-07-08 00:25:41 +0800

    feat (L001 - ListExampleView) : add properties

--- L001-List/List/List/ListExampleView.swift
+++ L001-List/List/List/ListExampleView.swift
@@ -8,6 +8,17 @@
 import SwiftUI
 
 struct ListExampleView: View {
+    
+    let options = ["One", "Two"]
+    
+    @State private var text = ""
+    @State private var password = ""
+    @State private var toggleOn = true
+    @State private var pickerSelection = 0
+    @State private var date = Date()
+    @State private var sliderValue = 4.0
+    @State private var stepperValue = 1
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
