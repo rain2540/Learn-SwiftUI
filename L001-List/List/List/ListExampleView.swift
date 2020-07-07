ed2f97cecf47c59fddca4841698c21200e5df2d4
Rain2540 <rain2540@foxmail.com>
2020-07-08 00:24:47 +0800

    chore (L001 - project) : add ListExampleView

---
+++ L001-List/List/List/ListExampleView.swift
@@ -0,0 +1,20 @@
+//
+//  ListExampleView.swift
+//  List
+//
+//  Created by Rain on 2020/7/8.
+//
+
+import SwiftUI
+
+struct ListExampleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListExampleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListExampleView()
+    }
+}
