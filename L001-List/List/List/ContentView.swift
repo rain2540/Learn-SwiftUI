4bd20089db3968d2f3270203e9a582bad469334f
Rain2540 <rain2540@foxmail.com>
2020-07-08 00:23:31 +0800

    chore (L001) : add files

---
+++ L001-List/List/List/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  List
+//
+//  Created by Rain on 2020/7/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
