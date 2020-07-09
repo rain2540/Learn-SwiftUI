f90161bb4290665cad372ebeb695137ad00df32b
Rain2540 <rain2540@foxmail.com>
2020-07-10 03:48:31 +0800

    chore (L003 - project) : add Home

---
+++ L003-ToDoList/ToDoList/ToDoList/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  ToDoList
+//
+//  Created by Rain on 2020/7/10.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
