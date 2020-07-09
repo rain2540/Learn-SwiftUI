20ec7e10bcf95caaedbbc79f138d31942f77bb52
Rain2540 <rain2540@foxmail.com>
2020-07-10 03:33:32 +0800

    chore (L003 - project) : add ToDoItem

---
+++ L003-ToDoList/ToDoList/ToDoList/ToDoItem.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoItem.swift
+//  ToDoList
+//
+//  Created by Rain on 2020/7/10.
+//
+
+import SwiftUI
+
+struct ToDoItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ToDoItem_Previews: PreviewProvider {
+    static var previews: some View {
+        ToDoItem()
+    }
+}
