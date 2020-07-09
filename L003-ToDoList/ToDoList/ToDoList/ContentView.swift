fa1b37f89cf31a9400ed98a851b034295a8b8877
Rain2540 <rain2540@foxmail.com>
2020-07-10 03:20:47 +0800

    chore (L003) : add L003 - ToDoList

---
+++ L003-ToDoList/ToDoList/ToDoList/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ToDoList
+//
+//  Created by Rain on 2020/7/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
