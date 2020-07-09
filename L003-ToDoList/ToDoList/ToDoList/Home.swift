5b8768bd5b74d5b39971044c47d07b4f2bfd7ff9
Rain2540 <rain2540@foxmail.com>
2020-07-10 03:49:33 +0800

    feat (L003 - Home) : add variables

--- L003-ToDoList/ToDoList/ToDoList/Home.swift
+++ L003-ToDoList/ToDoList/ToDoList/Home.swift
@@ -7,6 +7,11 @@
 
 import SwiftUI
 
+var editingMode: Bool = false
+var editingTodo: Todo = emptyTodo
+var editingIndex: Int = 0
+var detailsShouldUpdateTitle: Bool = false
+
 struct Home: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
