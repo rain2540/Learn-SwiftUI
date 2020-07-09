065a703112ee6f79ac04c8fa2379ed61dd593163
Rain2540 <rain2540@foxmail.com>
2020-07-10 03:40:48 +0800

    feat (L003 - ToDo) : subclass of NSObject

--- L003-ToDoList/ToDoList/ToDoList/ToDoItem.swift
+++ L003-ToDoList/ToDoList/ToDoList/ToDoItem.swift
@@ -7,15 +7,27 @@
 
 import SwiftUI
 
-class ToDo {
+class ToDo: NSObject, NSCoding {
+    func encode(with coder: NSCoder) {
+        coder.encode(self.title, forKey: "title")
+        coder.encode(self.dueDate, forKey: "dueDate")
+        coder.encode(self.checked, forKey: "checked")
+    }
+    
+    required init?(coder: NSCoder) {
+        self.title      =   coder.decodeObject(forKey: "title") as? String ?? ""
+        self.dueDate    =   coder.decodeObject(forKey: "dueDate") as? Date ?? Date()
+        self.checked    =   coder.decodeBool(forKey: "checked")
+    }
+    
     var title: String = ""
     var dueDate: Date = Date()
     var checked: Bool = false
     var index: Int = 0
     
     init(title: String, dueDate: Date) {
-        self.title = title
-        self.dueDate = dueDate
+        self.title      =   title
+        self.dueDate    =   dueDate
     }
 }
 
