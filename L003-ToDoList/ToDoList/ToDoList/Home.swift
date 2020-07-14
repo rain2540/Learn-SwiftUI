6f3f3fb6cad3d5092a1eaff172ec26673d1eb35b
Rain2540 <rain2540@foxmail.com>
2020-07-15 04:08:25 +0800

    feat (L003 - Home) : add class 'Main'

--- L003-ToDoList/ToDoList/ToDoList/Home.swift
+++ L003-ToDoList/ToDoList/ToDoList/Home.swift
@@ -12,6 +12,10 @@ var editingTodo: Todo = emptyTodo
 var editingIndex: Int = 0
 var detailsShouldUpdateTitle: Bool = false
 
+class Main: ObservableObject {
+
+}
+
 struct Home: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
