5340bf22b74056580d70001c5997f7ffd026a625
Rain2540 <rain2540@foxmail.com>
2020-07-15 23:14:18 +0800

    feat (L0002 - ContentView) : add preview group

--- L002-Calculator/Calculator/Calculator/ContentView.swift
+++ L002-Calculator/Calculator/Calculator/ContentView.swift
@@ -15,6 +15,10 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView().previewDevice("iPhone SE")
+            ContentView().previewDevice("iPad Air 2")
+        }
     }
 }
