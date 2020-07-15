69e061d57b8a49e510ba5648fa7f4f2577286559
Smartech <rain2540@foxmail.com>
2020-07-15 19:34:06 +0800

    chore (L004) : add L004 - PokeMaster

---
+++ L004-PokeMaster/PokeMaster/PokeMaster/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  PokeMaster
+//
+//  Created by RAIN on 2020/7/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
