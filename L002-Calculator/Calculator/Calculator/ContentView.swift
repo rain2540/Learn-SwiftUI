8c6008a79f32d469038b68c71da9969ffd7f263a
Rain2540 <rain2540@foxmail.com>
2020-07-15 23:00:36 +0800

    chore (L002) : add files

---
+++ L002-Calculator/Calculator/Calculator/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by Rain on 2020/7/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
