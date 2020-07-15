eddef08589a09b9b082e5fdec3d733751ba08b21
Smartech <rain2540@foxmail.com>
2020-07-15 19:37:50 +0800

    chore (L004 - project) : add PokemonInfoRow

---
+++ L004-PokeMaster/PokeMaster/PokeMaster/PokemonInfoRow.swift
@@ -0,0 +1,20 @@
+//
+//  PokemonInfoRow.swift
+//  PokeMaster
+//
+//  Created by RAIN on 2020/7/15.
+//
+
+import SwiftUI
+
+struct PokemonInfoRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PokemonInfoRow_Previews: PreviewProvider {
+    static var previews: some View {
+        PokemonInfoRow()
+    }
+}
