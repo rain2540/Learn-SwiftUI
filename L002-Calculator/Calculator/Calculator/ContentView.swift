a6ccfd0da532ac753979882108acf9bf3fe66816
Rain2540 <rain2540@foxmail.com>
2020-07-15 23:16:15 +0800

    feat (L002 - ContentView) : add CalculatorButton

--- L002-Calculator/Calculator/Calculator/ContentView.swift
+++ L002-Calculator/Calculator/Calculator/ContentView.swift
@@ -14,6 +14,28 @@ struct ContentView: View {
 }
 
 
+struct CalculatorButton: View {
+  
+  let fontSize: CGFloat = 38
+  let title: String
+  let size: CGSize
+  let backgroundColorName: String
+  let foregroundColor: Color
+  let action: () -> Void
+  
+  var body: some View {
+    Button(action: action) {
+      Text(title)
+        .font(.system(size: fontSize * scale))
+        .foregroundColor(foregroundColor)
+        .frame(width: size.width * scale, height: size.height * scale)
+        .background(Color(backgroundColorName))
+        .cornerRadius(size.width * scale / 2)
+    }
+  }
+}
+
+
 struct HistoryView: View {
     
     @ObservedObject var model: CalculatorModel
