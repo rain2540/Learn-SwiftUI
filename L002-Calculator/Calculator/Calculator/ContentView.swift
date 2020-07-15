3bd24bba2b3ebf17d48d69bb7007721ccf418119
Rain2540 <rain2540@foxmail.com>
2020-07-15 23:19:35 +0800

    feat (L002 - ContentView) : layout subviews

--- L002-Calculator/Calculator/Calculator/ContentView.swift
+++ L002-Calculator/Calculator/Calculator/ContentView.swift
@@ -10,33 +10,58 @@ import SwiftUI
 let scale: CGFloat = UIScreen.main.bounds.width / 414
 
 struct ContentView: View {
+    
+    //@State private var brain: CalculatorBrain = .left("0")
+    //@ObservedObject var model = CalculatorModel()
+    @EnvironmentObject var model: CalculatorModel
+    @State private var editingHistory = false
+    
     var body: some View {
-        Text("Hello, world!").padding()
+        VStack(spacing: 12) {
+            Spacer()
+            Button("操作履历：\(model.history.count)") {
+                self.editingHistory = true
+            }.sheet(isPresented: self.$editingHistory) {
+                HistoryView(model: self.model)
+            }
+            Text(model.brain.output)
+                .font(.system(size: 76))
+                .minimumScaleFactor(0.5)
+                .padding(.trailing, 24 * scale)
+                .lineLimit(1)
+                .frame(
+                    minWidth: 0,
+                    maxWidth: .infinity,
+                    alignment: .trailing)
+            CalculatorButtonPad()
+                .padding(.bottom)
+        }
     }
+    
 }
 
 
 struct CalculatorButtonPad: View {
-  
-  //@Binding var brain: CalculatorBrain
-  //var model: CalculatorModel
-  
-  let pad: [[CalculatorButtonItem]] = [
-    [.command(.clear), .command(.flip), .command(.percent), .op(.divide),],
-    [.digit(7), .digit(8), .digit(9), .op(.multiply),],
-    [.digit(4), .digit(5), .digit(6), .op(.minus),],
-    [.digit(1), .digit(2), .digit(3), .op(.plus),],
-    [.digit(0), .dot, .op(.equal),],
-  ]
-  
-  var body: some View {
-    VStack(spacing: 8) {
-      ForEach(pad, id: \.self) { row in
-        CalculatorButtonRow(row: row)
-      }
+    
+    //@Binding var brain: CalculatorBrain
+    //var model: CalculatorModel
+    
+    let pad: [[CalculatorButtonItem]] = [
+        [.command(.clear), .command(.flip), .command(.percent), .op(.divide),],
+        [.digit(7), .digit(8), .digit(9), .op(.multiply),],
+        [.digit(4), .digit(5), .digit(6), .op(.minus),],
+        [.digit(1), .digit(2), .digit(3), .op(.plus),],
+        [.digit(0), .dot, .op(.equal),],
+    ]
+    
+    var body: some View {
+        VStack(spacing: 8) {
+            ForEach(pad, id: \.self) { row in
+                CalculatorButtonRow(row: row)
+            }
+        }
     }
-  }
-  
+    
 }
 
 
