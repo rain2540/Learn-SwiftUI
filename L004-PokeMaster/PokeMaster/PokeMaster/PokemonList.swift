13988d974817134781d7f9638128110ffe34023b
Smartech <rain2540@foxmail.com>
2020-08-03 13:43:20 +0800

    chore (L004 - project) : add PokemonList

---
+++ L004-PokeMaster/PokeMaster/PokeMaster/PokemonList.swift
@@ -0,0 +1,20 @@
+//
+//  PokemonList.swift
+//  PokeMaster
+//
+//  Created by RAIN on 2020/8/3.
+//
+
+import SwiftUI
+
+struct PokemonList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PokemonList_Previews: PreviewProvider {
+    static var previews: some View {
+        PokemonList()
+    }
+}
