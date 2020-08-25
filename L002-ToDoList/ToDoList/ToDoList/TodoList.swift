b10e98787893f925fe12f5f54e34036ad278494e
Smartech <rain2540@foxmail.com>
2020-08-25 16:11:21 +0800

    chore (L002 - Project) : Calculator -> TodoList

---
+++ L002-ToDoList/ToDoList/ToDoList/TodoList.swift
@@ -0,0 +1,20 @@
+//
+//  TodoList.swift
+//  ToDoList
+//
+//  Created by Rain on 2020/7/16.
+//
+
+import SwiftUI
+
+struct TodoList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TodoList_Previews: PreviewProvider {
+    static var previews: some View {
+        TodoList()
+    }
+}
