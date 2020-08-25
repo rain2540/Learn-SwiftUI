63a6fd481bf09a9d2f73c263cfc17072e617dbd4
Smartech <rain2540@foxmail.com>
2020-08-25 16:38:49 +0800

    feat (L003 - ContentView) : alignment for VStack

--- L003-Landmarks/Landmarks/Landmarks/ContentView.swift
+++ L003-Landmarks/Landmarks/Landmarks/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
             Text("Turtle Rock")
                 .font(.title)
                 .foregroundColor(.green)
