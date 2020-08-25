2d5ba50e4317132425d98e4a0dde2b4b6916c511
Smartech <rain2540@foxmail.com>
2020-08-25 16:20:28 +0800

    chore (L005 - Project) : PokeMaster L004 -> L005

---
+++ L005-PokeMaster/PokeMaster/PokeMaster/PokemonList.swift
@@ -0,0 +1,46 @@
+//
+//  PokemonList.swift
+//  PokeMaster
+//
+//  Created by RAIN on 2020/8/3.
+//
+
+import SwiftUI
+
+struct PokemonList: View {
+
+    @State var expandingIndex: Int?
+
+    var body: some View {
+        ScrollView {
+            ForEach(PokemonViewModel.all) { pokemon in
+                PokemonInfoRow(
+                    model: pokemon,
+                    expanded: self.expandingIndex == pokemon.id
+                )
+                    .onTapGesture {
+                        withAnimation(
+                            .spring(
+                                response: 0.55,
+                                dampingFraction: 0.425,
+                                blendDuration: 0)
+                            )
+                        {
+                            if self.expandingIndex == pokemon.id {
+                                self.expandingIndex = nil
+                            } else {
+                                self.expandingIndex = pokemon.id
+                            }
+                        }
+                }
+            }
+        }
+    }
+
+}
+
+struct PokemonList_Previews: PreviewProvider {
+    static var previews: some View {
+        PokemonList()
+    }
+}
