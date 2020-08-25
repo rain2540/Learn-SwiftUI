36db86c2ca13e30cbd9c5df00ac16144b8082b69
Smartech <rain2540@foxmail.com>
2020-08-25 16:50:04 +0800

    chore (L003 - project) : add CircleImage

---
+++ L003-Landmarks/Landmarks/Landmarks/CircleImage.swift
@@ -0,0 +1,21 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by RAIN on 2020/8/25.
+//  Copyright © 2020 Smartech. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
