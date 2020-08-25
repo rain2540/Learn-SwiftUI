69b5cdbb557ff4c3070e0e345e50dc9f35839248
Smartech <rain2540@foxmail.com>
2020-08-25 16:34:36 +0800

    feat (L003 - ContentView) : text into VStack

--- L003-Landmarks/Landmarks/Landmarks/ContentView.swift
+++ L003-Landmarks/Landmarks/Landmarks/ContentView.swift
@@ -10,9 +10,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Turtle Rock")
-            .font(.title)
-            .foregroundColor(.green)
+        VStack {
+            Text("Turtle Rock")
+                .font(.title)
+                .foregroundColor(.green)
+        }
     }
 }
 
