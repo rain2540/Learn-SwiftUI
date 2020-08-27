e8114f2646788fb78f74a4c7bdd8b5d4b3a7daf4
Smartech <rain2540@foxmail.com>
2020-08-27 16:29:15 +0800

    feat (L003 - MapView) : sets the map view’s region
     - sets the map view’s region to the correct coordinates to center the map on Turtle Rock.

--- L003-Landmarks/Landmarks/Landmarks/MapView.swift
+++ L003-Landmarks/Landmarks/Landmarks/MapView.swift
@@ -15,6 +15,14 @@ struct MapView: UIViewRepresentable {
         MKMapView(frame: .zero)
     }
 
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        let coordinate = CLLocationCoordinate2D(
+            latitude: 34.011286, longitude: -116.166868)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        uiView.setRegion(region, animated: true)
+    }
+
 }
 
 struct MapView_Previews: PreviewProvider {
