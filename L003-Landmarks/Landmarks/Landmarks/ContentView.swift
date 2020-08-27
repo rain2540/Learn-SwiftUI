19c02c145e7f492c13bda60853ca4c88fc52cc22
Smartech <rain2540@foxmail.com>
2020-08-27 16:36:41 +0800

    feat (L003 - ContentView) : Embed a VStack that holds the three text views in another VStack

--- L003-Landmarks/Landmarks/Landmarks/ContentView.swift
+++ L003-Landmarks/Landmarks/Landmarks/ContentView.swift
@@ -10,19 +10,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-                .foregroundColor(.green)
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
+        VStack {
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
             }
+            .padding()
         }
-        .padding()
     }
 }
 
