5633f0c9b089d0fe976f41582d1cd40ed5d09f9c
Smartech <rain2540@foxmail.com>
2020-08-27 17:24:36 +0800

    feat (L003 - LandmarkRow) : Add landmark as a stored property of LandmarkRow.
     - When you add the landmark property, the preview stops working, because the LandmarkRow type needs a landmark instance during initialization.

--- L003-Landmarks/Landmarks/Landmarks/LandmarkRow.swift
+++ L003-Landmarks/Landmarks/Landmarks/LandmarkRow.swift
@@ -9,6 +9,9 @@
 import SwiftUI
 
 struct LandmarkRow: View {
+
+    var landmark: Landmark
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
