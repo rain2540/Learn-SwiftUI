404845fd7b87cd30c56a87737e92e9fc1000d344
Smartech <rain2540@foxmail.com>
2020-08-27 16:22:58 +0800

    feat (L003 - MapView) : import MapKit

--- L003-Landmarks/Landmarks/Landmarks/MapView.swift
+++ L003-Landmarks/Landmarks/Landmarks/MapView.swift
@@ -7,8 +7,9 @@
 //
 
 import SwiftUI
+import MapKit
 
-struct MapView: View {
+struct MapView: UIViewRepresentable {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
