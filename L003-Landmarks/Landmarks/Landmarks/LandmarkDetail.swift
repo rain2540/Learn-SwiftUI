0dc832be2ec892d44bb67535a8b9d74ac1aa0849
Smartech <rain2540@foxmail.com>
2020-09-01 17:08:47 +0800

    refactor (L003 - LandmarkDetail) : update layout

--- L003-Landmarks/Landmarks/Landmarks/LandmarkDetail.swift
+++ L003-Landmarks/Landmarks/Landmarks/LandmarkDetail.swift
@@ -19,13 +19,14 @@ struct LandmarkDetail: View {
                 .frame(height: 300)
 
             CircleImage(image: landmark.image)
-                .offset(y: -130)
+                .offset(x: 0, y: -130)
                 .padding(.bottom, -130)
 
             VStack(alignment: .leading) {
                 Text(landmark.name)
                     .font(.title)
-                HStack {
+
+                HStack(alignment: .top) {
                     Text(landmark.park)
                         .font(.subheadline)
                     Spacer()
