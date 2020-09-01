af993ad2547dcd70d4c60d9f73918c7d4d543294
Smartech <rain2540@foxmail.com>
2020-09-01 09:23:49 +0800

    chore (L003 - project) : Create a new SwiftUI view, named LandmarkList.swift.

---
+++ L003-Landmarks/Landmarks/Landmarks/LandmarkList.swift
@@ -0,0 +1,21 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by RAIN on 2020/9/1.
+//  Copyright © 2020 Smartech. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
