3000d2d5e4a6dec1c46492bc0c23c97bb4a3f544
Smartech <rain2540@foxmail.com>
2020-09-01 09:11:50 +0800

    feat (L003 - LandmarkRow) : To simplify the code, move the previewLayout(_:) call to the outside of the group’s child declarations.
     - A view’s children inherit the view’s contextual settings, such as preview configurations.

--- L003-Landmarks/Landmarks/Landmarks/LandmarkRow.swift
+++ L003-Landmarks/Landmarks/Landmarks/LandmarkRow.swift
@@ -27,9 +27,8 @@ struct LandmarkRow_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             LandmarkRow(landmark: landmarkData[0])
-                .previewLayout(.fixed(width: 300, height: 70))
             LandmarkRow(landmark: landmarkData[1])
-                .previewLayout(.fixed(width: 300, height: 70))
         }
+        .previewLayout(.fixed(width: 300, height: 70))
     }
 }
