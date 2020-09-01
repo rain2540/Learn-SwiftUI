a044376c195e29d9a6857d68b401452a1e93cace
Smartech <rain2540@foxmail.com>
2020-09-01 17:12:37 +0800

    feat (L003 - LandmarkRow) : Because system images are vector based, you can change their color with the foregroundColor(_:) modifier.
     - The star is present whenever a landmark’s isFavorite property is true. You’ll see how to modify that property later in this tutorial.

--- L003-Landmarks/Landmarks/Landmarks/LandmarkRow.swift
+++ L003-Landmarks/Landmarks/Landmarks/LandmarkRow.swift
@@ -23,6 +23,7 @@ struct LandmarkRow: View {
             if landmark.isFavorite {
                 Image(systemName: "star.fill")
                     .imageScale(.medium)
+                    .foregroundColor(.yellow)
             }
         }
     }
