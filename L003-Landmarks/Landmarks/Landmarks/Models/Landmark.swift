329861c217e796f17dbd9bd183e1f52bc23b3ef5
Smartech <rain2540@foxmail.com>
2020-09-01 17:07:36 +0800

    refactor (L003 - Landmark) : add a new category

--- L003-Landmarks/Landmarks/Landmarks/Models/Landmark.swift
+++ L003-Landmarks/Landmarks/Landmarks/Models/Landmark.swift
@@ -29,6 +29,7 @@ struct Landmark: Hashable, Codable, Identifiable {
         case featured = "Featured"
         case lakes = "Lakes"
         case rivers = "Rivers"
+        case mountains = "Mountains"
     }
 }
 
