d79f0c5a5050f59734789db32825aa45563eb3e3
Smartech <rain2540@foxmail.com>
2020-09-01 09:09:40 +0800

    feat (L003 - LandmarkRow) : Wrap the returned row in a Group, and add the first row back again.
     - Group is a container for grouping view content. Xcode renders the group’s child views as separate previews in the canvas.

--- L003-Landmarks/Landmarks/Landmarks/LandmarkRow.swift
+++ L003-Landmarks/Landmarks/Landmarks/LandmarkRow.swift
@@ -25,7 +25,11 @@ struct LandmarkRow: View {
 
 struct LandmarkRow_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkRow(landmark: landmarkData[0])
-            .previewLayout(.fixed(width: 300, height: 70))
+        Group {
+            LandmarkRow(landmark: landmarkData[0])
+                .previewLayout(.fixed(width: 300, height: 70))
+            LandmarkRow(landmark: landmarkData[1])
+                .previewLayout(.fixed(width: 300, height: 70))
+        }
     }
 }
