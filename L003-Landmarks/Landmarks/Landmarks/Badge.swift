f860990cc6129c043cfc39e8e68b4b7877702da5
Smartech <rain2540@foxmail.com>
2020-09-03 15:35:25 +0800

    feat (L003 - BAdge) : Add a starting point to the path.
     - The move(to:) method moves the drawing cursor within the bounds of a shape as though an imaginary pen or pencil is hovering over the area, waiting to start drawing.

--- L003-Landmarks/Landmarks/Landmarks/Badge.swift
+++ L003-Landmarks/Landmarks/Landmarks/Badge.swift
@@ -12,7 +12,9 @@ struct Badge: View {
 
     var body: some View {
         Path { path in
-
+            var width: CGFloat = 100.0
+            let height = width
+            path.move(to: CGPoint(x: width * 0.95, y: height * 0.2))
         }
         .fill(Color.black)
     }
