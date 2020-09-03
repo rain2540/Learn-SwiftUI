3df1def29af7575e51dbe560d02b69a3ef16cda3
Smartech <rain2540@foxmail.com>
2020-09-03 15:22:22 +0800

    chore (L003 - project) : add Badge.swift

---
+++ L003-Landmarks/Landmarks/Landmarks/Badge.swift
@@ -0,0 +1,21 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by RAIN on 2020/9/3.
+//  Copyright © 2020 Smartech. All rights reserved.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
