c637e12cf2e4a380aa4d90eaf2b4e60d6ca51df0
Smartech <rain2540@foxmail.com>
2020-09-03 15:51:10 +0800

    feat (L003 - Badge) : Wrap the badge path in a GeometryReader so the badge can use the size of its containing view, which defines the size instead of hard-coding the value (100).
     - Using the smallest of the geometry’s two dimensions preserves the aspect ratio of the badge when its containing view isn’t square.

--- L003-Landmarks/Landmarks/Landmarks/Badge.swift
+++ L003-Landmarks/Landmarks/Landmarks/Badge.swift
@@ -11,37 +11,39 @@ import SwiftUI
 struct Badge: View {
 
     var body: some View {
-        Path { path in
-            var width: CGFloat = 100.0
-            let height = width
-            path.move(
-                to: CGPoint(
-                    x: width * 0.95,
-                    y: height * (0.20 + HexagonParameters.adjustment)
-                )
-            )
-
-            HexagonParameters.points.forEach {
-                path.addLine(
-                    to: .init(
-                        x: width * $0.useWidth.0 * $0.xFactors.0,
-                        y: height * $0.useHeight.0 * $0.yFactors.0
+        GeometryReader { geometry in
+            Path { path in
+                var width: CGFloat = min(geometry.size.width, geometry.size.height)
+                let height = width
+                path.move(
+                    to: CGPoint(
+                        x: width * 0.95,
+                        y: height * (0.20 + HexagonParameters.adjustment)
                     )
                 )
 
-                path.addQuadCurve(
-                    to: .init(
-                        x: width * $0.useWidth.1 * $0.xFactors.1,
-                        y: width * $0.useHeight.1 * $0.yFactors.1
-                    ),
-                    control: .init(
-                        x: width * $0.useWidth.2 * $0.xFactors.2,
-                        y: width * $0.useHeight.2 * $0.yFactors.2
+                HexagonParameters.points.forEach {
+                    path.addLine(
+                        to: .init(
+                            x: width * $0.useWidth.0 * $0.xFactors.0,
+                            y: height * $0.useHeight.0 * $0.yFactors.0
+                        )
                     )
-                )
+
+                    path.addQuadCurve(
+                        to: .init(
+                            x: width * $0.useWidth.1 * $0.xFactors.1,
+                            y: width * $0.useHeight.1 * $0.yFactors.1
+                        ),
+                        control: .init(
+                            x: width * $0.useWidth.2 * $0.xFactors.2,
+                            y: width * $0.useHeight.2 * $0.yFactors.2
+                        )
+                    )
+                }
             }
+            .fill(Color.black)
         }
-        .fill(Color.black)
     }
 
 }
