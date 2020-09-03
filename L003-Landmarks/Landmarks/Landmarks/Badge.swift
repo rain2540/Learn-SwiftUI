17f3bdc4159f760909a775db1643796fc587626c
Smartech <rain2540@foxmail.com>
2020-09-03 15:45:42 +0800

    feat (L003 - Badge) : Use the addQuadCurve(to:control:) method to draw the Bézier curves for the badge’s corners.

--- L003-Landmarks/Landmarks/Landmarks/Badge.swift
+++ L003-Landmarks/Landmarks/Landmarks/Badge.swift
@@ -14,13 +14,29 @@ struct Badge: View {
         Path { path in
             var width: CGFloat = 100.0
             let height = width
-            path.move(to: CGPoint(x: width * 0.95, y: height * 0.2))
+            path.move(
+                to: CGPoint(
+                    x: width * 0.95,
+                    y: height * (0.20 + HexagonParameters.adjustment)
+                )
+            )
 
             HexagonParameters.points.forEach {
                 path.addLine(
                     to: .init(
                         x: width * $0.useWidth.0 * $0.xFactors.0,
                         y: height * $0.useHeight.0 * $0.yFactors.0
+                    )
+                )
+
+                path.addQuadCurve(
+                    to: .init(
+                        x: width * $0.useWidth.1 * $0.xFactors.1,
+                        y: width * $0.useHeight.1 * $0.yFactors.1
+                    ),
+                    control: .init(
+                        x: width * $0.useWidth.2 * $0.xFactors.2,
+                        y: width * $0.useHeight.2 * $0.yFactors.2
                     )
                 )
             }
