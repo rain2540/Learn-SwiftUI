c31046b2268698f70cec8ebeb830e85e31d22ac6
Smartech <rain2540@foxmail.com>
2020-09-03 15:40:11 +0800

    feat (L003 - Badge) : Draw the lines for each point of the shape data to create a rough hexagonal shape.
     - The addLine(to:) method takes a single point and draws it. Successive calls to addLine(to:) begin a line at the previous point and continue to the new point.
     - Don’t worry if your hexagon looks a little unusual; that’s expected behavior right now. Over the next few steps, you’ll work toward making the hexagon shape appear more like the badge shape shown at the beginning of this section.

--- L003-Landmarks/Landmarks/Landmarks/Badge.swift
+++ L003-Landmarks/Landmarks/Landmarks/Badge.swift
@@ -15,6 +15,15 @@ struct Badge: View {
             var width: CGFloat = 100.0
             let height = width
             path.move(to: CGPoint(x: width * 0.95, y: height * 0.2))
+
+            HexagonParameters.points.forEach {
+                path.addLine(
+                    to: .init(
+                        x: width * $0.useWidth.0 * $0.xFactors.0,
+                        y: height * $0.useHeight.0 * $0.yFactors.0
+                    )
+                )
+            }
         }
         .fill(Color.black)
     }
