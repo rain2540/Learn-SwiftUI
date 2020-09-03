13f66419efa1268c1485cb5a22762f6478240e1e
Smartech <rain2540@foxmail.com>
2020-09-03 10:30:36 +0800

    feat (L003 - LandmarkDetail) : Create a new button next to the landmark’s name.
     - Use an if-else conditional statement to provide different images that indicate whether the landmark is a favorite.
     - In the button’s action closure, the code uses landmarkIndex with the userData object to update the landmark in place.

--- L003-Landmarks/Landmarks/Landmarks/LandmarkDetail.swift
+++ L003-Landmarks/Landmarks/Landmarks/LandmarkDetail.swift
@@ -31,6 +31,18 @@ struct LandmarkDetail: View {
                 HStack {
                     Text(landmark.name)
                         .font(.title)
+
+                    Button(action: {
+                        self.userData.landmarks[self.landmarkIndex].isFavorite.toggle()
+                    }) {
+                        if self.userData.landmarks[self.landmarkIndex].isFavorite {
+                            Image(systemName: "star.fill")
+                                .foregroundColor(Color.yellow)
+                        } else {
+                            Image(systemName: "star")
+                                .foregroundColor(Color.gray)
+                        }
+                    }
                 }
 
                 HStack(alignment: .top) {
