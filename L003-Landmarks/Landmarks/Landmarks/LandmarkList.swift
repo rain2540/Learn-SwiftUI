72573b661a3778d62bd91ed01bc0806d34ceadcd
Smartech <rain2540@foxmail.com>
2020-09-03 09:06:02 +0800

    feat (L003 - LandmarkList) : Create a nested ForEach group to transform the landmarks into rows.
     - To combine static and dynamic views in a list, or to combine two or more different groups of dynamic views, use the ForEach type instead of passing your collection of data to List.

--- L003-Landmarks/Landmarks/Landmarks/LandmarkList.swift
+++ L003-Landmarks/Landmarks/Landmarks/LandmarkList.swift
@@ -10,15 +10,17 @@ import SwiftUI
 
 struct LandmarkList: View {
 
-    @State var showFavoritesOnly = false
+    @State var showFavoritesOnly = true
 
     var body: some View {
 
         NavigationView {
-            List(landmarkData) { landmark in
-                if !self.showFavoritesOnly || landmark.isFavorite {
-                    NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
-                        LandmarkRow(landmark: landmark)
+            List {
+                ForEach(landmarkData) { landmark in
+                    if !self.showFavoritesOnly || landmark.isFavorite {
+                        NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                            LandmarkRow(landmark: landmark)
+                        }
                     }
                 }
             }
