f44e1b20e8f4b0ba557f1fd414a1ceb85c7a39d1
Smartech <rain2540@foxmail.com>
2020-09-04 16:16:24 +0800

    chore (L003 - project) : Create a new custom view called BadgeSymbol for the mountain shape that’s stamped in a rotated pattern in the badge design.

---
+++ L003-Landmarks/Landmarks/Landmarks/BadgeSymbol.swift
@@ -0,0 +1,21 @@
+//
+//  BadgeSymbol.swift
+//  Landmarks
+//
+//  Created by RAIN on 2020/9/4.
+//  Copyright © 2020 Smartech. All rights reserved.
+//
+
+import SwiftUI
+
+struct BadgeSymbol: View {
+    var body: some View {
+        Text("Badge Symbol")
+    }
+}
+
+struct BadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeSymbol()
+    }
+}
