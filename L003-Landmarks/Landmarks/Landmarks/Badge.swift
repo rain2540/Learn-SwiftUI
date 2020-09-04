d5b662a1725970e267ade9484658e387d207b35c
Smartech <rain2540@foxmail.com>
2020-09-04 15:26:56 +0800

    feat (L003 - Badge) : Center the badge within its geometry with the xScale and xOffset adjustment variables.

--- L003-Landmarks/Landmarks/Landmarks/Badge.swift
+++ L003-Landmarks/Landmarks/Landmarks/Badge.swift
@@ -15,9 +15,12 @@ struct Badge: View {
             Path { path in
                 var width: CGFloat = min(geometry.size.width, geometry.size.height)
                 let height = width
+                let xScale: CGFloat = 0.832
+                let xOffset = (width * (1.0 - xScale)) / 2.0
+                width *= xScale
                 path.move(
                     to: CGPoint(
-                        x: width * 0.95,
+                        x: xOffset + width * 0.95,
                         y: height * (0.20 + HexagonParameters.adjustment)
                     )
                 )
@@ -25,19 +28,19 @@ struct Badge: View {
                 HexagonParameters.points.forEach {
                     path.addLine(
                         to: .init(
-                            x: width * $0.useWidth.0 * $0.xFactors.0,
+                            x: xOffset + width * $0.useWidth.0 * $0.xFactors.0,
                             y: height * $0.useHeight.0 * $0.yFactors.0
                         )
                     )
 
                     path.addQuadCurve(
                         to: .init(
-                            x: width * $0.useWidth.1 * $0.xFactors.1,
-                            y: width * $0.useHeight.1 * $0.yFactors.1
+                            x: xOffset + width * $0.useWidth.1 * $0.xFactors.1,
+                            y: height * $0.useHeight.1 * $0.yFactors.1
                         ),
                         control: .init(
-                            x: width * $0.useWidth.2 * $0.xFactors.2,
-                            y: width * $0.useHeight.2 * $0.yFactors.2
+                            x: xOffset + width * $0.useWidth.2 * $0.xFactors.2,
+                            y: height * $0.useHeight.2 * $0.yFactors.2
                         )
                     )
                 }
