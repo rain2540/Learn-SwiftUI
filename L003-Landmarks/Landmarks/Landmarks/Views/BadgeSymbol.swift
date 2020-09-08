707ce546db30c4f456ece37ae12f914410e69f49
Smartech <rain2540@foxmail.com>
2020-09-08 08:24:18 +0800

    feat (L003- BadgeSymbol) : Draw the top portion of the symbol using the path APIs.
     * Experiment
     * Try adjusting the numeric multipliers associated with the spacing, topWidth, and topHeight constants to see how they influence the overall shape.

--- L003-Landmarks/Landmarks/Landmarks/Views/BadgeSymbol.swift
+++ L003-Landmarks/Landmarks/Landmarks/Views/BadgeSymbol.swift
@@ -9,9 +9,28 @@
 import SwiftUI
 
 struct BadgeSymbol: View {
+
     var body: some View {
-        Text("Badge Symbol")
+        GeometryReader { geometry in
+            Path { path in
+                let width = min(geometry.size.width, geometry.size.height)
+                let height = width * 0.75
+                let spacing = width * 0.030
+                let middle = width / 2
+                let topWidth = 0.226 * width
+                let topHeight = 0.488 * height
+
+                path.addLines([
+                    CGPoint(x: middle, y: spacing),
+                    CGPoint(x: middle - topWidth, y: topHeight - spacing),
+                    CGPoint(x: middle, y: topHeight / 2 + spacing),
+                    CGPoint(x: middle + topWidth, y: topHeight - spacing),
+                    CGPoint(x: middle, y: spacing),
+                ])
+            }
+        }
     }
+
 }
 
 struct BadgeSymbol_Previews: PreviewProvider {
