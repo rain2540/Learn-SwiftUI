79a0d863c6fa0bc5d2ac12963010bd567cd2052a
Smartech <rain2540@foxmail.com>
2020-09-08 16:37:59 +0800

    chore (L003 - project) : add files
     - hikeData.json
     - GraphCapsule.swift
     - HikeGraph.swift

---
+++ L003-Landmarks/Landmarks/Landmarks/Views/GraphCapsule.swift
@@ -0,0 +1,21 @@
+//
+//  GraphCapsule.swift
+//  Landmarks
+//
+//  Created by RAIN on 2020/9/8.
+//  Copyright © 2020 Smartech. All rights reserved.
+//
+
+import SwiftUI
+
+struct GraphCapsule: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GraphCapsule_Previews: PreviewProvider {
+    static var previews: some View {
+        GraphCapsule()
+    }
+}
---
+++ L003-Landmarks/Landmarks/Landmarks/Views/HikeGraph.swift
@@ -0,0 +1,21 @@
+//
+//  HikeGraph.swift
+//  Landmarks
+//
+//  Created by RAIN on 2020/9/8.
+//  Copyright © 2020 Smartech. All rights reserved.
+//
+
+import SwiftUI
+
+struct HikeGraph: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HikeGraph_Previews: PreviewProvider {
+    static var previews: some View {
+        HikeGraph()
+    }
+}
