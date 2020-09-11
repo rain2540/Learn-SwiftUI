ff6972acbe91719da5e5e4bd0dbecad0e324187e
Smartech <rain2540@foxmail.com>
2020-09-11 15:37:58 +0800

    chore (L003 - project) : add HikeDetail

---
+++ L003-Landmarks/Landmarks/Landmarks/HikeDetail.swift
@@ -0,0 +1,21 @@
+//
+//  HikeDetail.swift
+//  Landmarks
+//
+//  Created by RAIN on 2020/9/11.
+//  Copyright © 2020 Smartech. All rights reserved.
+//
+
+import SwiftUI
+
+struct HikeDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HikeDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        HikeDetail()
+    }
+}
