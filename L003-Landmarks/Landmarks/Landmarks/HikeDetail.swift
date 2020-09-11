0e0d52dc311cec8338a4302b49f00b60787ab3c6
Smartech <rain2540@foxmail.com>
2020-09-11 15:40:24 +0800

    feat (L003 - HikeDetail) : add content

--- L003-Landmarks/Landmarks/Landmarks/HikeDetail.swift
+++ L003-Landmarks/Landmarks/Landmarks/HikeDetail.swift
@@ -9,13 +9,42 @@
 import SwiftUI
 
 struct HikeDetail: View {
+
+    let hike: Hike
+    @State var dataToShow = \Hike.Observation.elevation
+
+    var buttons = [
+        ("Elevation", \Hike.Observation.elevation),
+        ("Heart Rate", \Hike.Observation.heartRate),
+        ("Pace", \Hike.Observation.pace),
+    ]
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        return VStack {
+            HikeGraph(hike: hike, path: dataToShow)
+                .frame(height: 200)
+
+            HStack(spacing: 25) {
+                ForEach(buttons, id: \.0) { value in
+                    Button(action: {
+                        self.dataToShow = value.1
+                    }) {
+                        Text(value.0)
+                            .font(.system(size: 15))
+                            .foregroundColor(value.1 == self.dataToShow
+                                ? Color.gray
+                                : Color.accentColor)
+                            .animation(nil)
+                    }
+                }
+            }
+        }
     }
+
 }
 
 struct HikeDetail_Previews: PreviewProvider {
     static var previews: some View {
-        HikeDetail()
+        HikeDetail(hike: hikeData[0])
     }
 }
