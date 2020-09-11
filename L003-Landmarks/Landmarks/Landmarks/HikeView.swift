f41c3e9f096331412c9f771864494f1d8ed448af
Smartech <rain2540@foxmail.com>
2020-09-11 16:43:35 +0800

    feat (L003 - HikeView) : Use the asymmetric(insertion:removal:) modifier to provide different transitions for when the view appears and disappears.

--- L003-Landmarks/Landmarks/Landmarks/HikeView.swift
+++ L003-Landmarks/Landmarks/Landmarks/HikeView.swift
@@ -11,7 +11,11 @@ import SwiftUI
 extension AnyTransition {
 
     static var moveAndFade: AnyTransition {
-        AnyTransition.move(edge: .trailing)
+        let insertion = AnyTransition.move(edge: .trailing)
+            .combined(with: .opacity)
+        let removal = AnyTransition.scale
+            .combined(with: .opacity)
+        return .asymmetric(insertion: insertion, removal: removal)
     }
 
 }
