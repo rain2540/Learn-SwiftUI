8f8719fa320f7f22c3efb1087f60c77574d1b9dc
Smartech <rain2540@foxmail.com>
2020-09-11 15:48:28 +0800

    chore (L003 - project) : add HikeView

---
+++ L003-Landmarks/Landmarks/Landmarks/HikeView.swift
@@ -0,0 +1,21 @@
+//
+//  HikeView.swift
+//  Landmarks
+//
+//  Created by RAIN on 2020/9/11.
+//  Copyright © 2020 Smartech. All rights reserved.
+//
+
+import SwiftUI
+
+struct HikeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HikeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HikeView()
+    }
+}
