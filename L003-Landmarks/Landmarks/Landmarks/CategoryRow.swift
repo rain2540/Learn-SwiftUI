2c9c9f9974eb724eaf43592e89a66877e959841f
Smartech <rain2540@foxmail.com>
2020-09-30 15:22:16 +0800

    feat (L003 - CategoryRow) : Give the rows room to breathe by specifying a tall frame(width:height:) and wrapping the stack in a scroll view.
     - Updating the view preview with a larger sampling of data makes it easier to ensure that the scrolling behavior is correct.

--- L003-Landmarks/Landmarks/Landmarks/CategoryRow.swift
+++ L003-Landmarks/Landmarks/Landmarks/CategoryRow.swift
@@ -14,12 +14,21 @@ struct CategoryRow: View {
     var items: [Landmark]
 
     var body: some View {
-        HStack(alignment: .top, spacing: 0, content: {
-            ForEach(self.items) { landmark in
-                Text(landmark.name)
-            }
-        })
+        VStack(alignment: .leading, content: {
+            Text(self.categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 5)
 
+            ScrollView(.horizontal, showsIndicators: false, content: {
+                HStack(alignment: .top, spacing: 0, content: {
+                    ForEach(self.items) { landmark in
+                        Text(landmark.name)
+                    }
+                })
+            })
+            .frame(height: 185)
+        })
     }
 
 }
@@ -28,7 +37,7 @@ struct CategoryRow_Previews: PreviewProvider {
     static var previews: some View {
         CategoryRow(
             categoryName: landmarkData[0].category.rawValue,
-            items: Array(landmarkData.prefix(3))
+            items: Array(landmarkData.prefix(4))
         )
     }
 }
