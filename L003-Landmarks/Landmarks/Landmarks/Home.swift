4f6dbc70292288ba1efb423a1a58f61039b745e7
Smartech <rain2540@foxmail.com>
2020-09-30 14:06:41 +0800

    chore (L003 - project) : Create a custom view called CategoryHome in a new file, named Home.swift.

---
+++ L003-Landmarks/Landmarks/Landmarks/Home.swift
@@ -0,0 +1,21 @@
+//
+//  Home.swift
+//  Landmarks
+//
+//  Created by RAIN on 2020/9/30.
+//  Copyright © 2020 Smartech. All rights reserved.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        Text("Landmarks Content")
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
