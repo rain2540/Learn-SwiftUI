0ac4d7d16ff66162e549c7495cae812a97ab49d9
Smartech <rain2540@foxmail.com>
2020-09-30 14:25:04 +0800

    feat (L003 - Home) : Group landmarks into categories using the Dictionary structure’s init(grouping:by:) initializer, keying off of the landmarks’ category property.
     - The starter project file includes predefined categories for each of the sample landmarks.

--- L003-Landmarks/Landmarks/Landmarks/Home.swift
+++ L003-Landmarks/Landmarks/Landmarks/Home.swift
@@ -9,12 +9,21 @@
 import SwiftUI
 
 struct CategoryHome: View {
+
+    var categories: [String: [Landmark]] {
+        Dictionary(
+            grouping: landmarkData,
+            by: { $0.category.rawValue }
+        )
+    }
+
     var body: some View {
         NavigationView {
             Text("Landmarks Content")
                 .navigationBarTitle(Text("Featured"))
         }
     }
+
 }
 
 struct CategoryHome_Previews: PreviewProvider {
