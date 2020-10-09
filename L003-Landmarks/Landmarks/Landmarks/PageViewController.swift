034beaed66fbfb848051ea84b199c2f72d7bc7a6
Smartech <rain2540@foxmail.com>
2020-10-09 10:48:33 +0800

    feat (L003 - project) : Create a new SwiftUI view file, named PageViewController.swift, and declare the PageViewController type as conforming to UIViewControllerRepresentable.
     - The page view controller stores an array of UIViewController instances. These are the pages for scrolling between landmarks.
    
     - Next, add the two requirements for the UIViewControllerRepresentable protocol.

---
+++ L003-Landmarks/Landmarks/Landmarks/PageViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PageViewController.swift
+//  Landmarks
+//
+//  Created by RAIN on 2020/10/9.
+//  Copyright © 2020 Smartech. All rights reserved.
+//
+
+import SwiftUI
+import UIKit
+
+struct PageViewController: UIViewControllerRepresentable {
+
+    var controllers: [UIViewController]
+
+}
