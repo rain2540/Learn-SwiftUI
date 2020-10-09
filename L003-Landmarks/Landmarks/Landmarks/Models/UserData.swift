8ad6fc6956c6ff1e373de32e3a70a78598bfd893
Smartech <rain2540@foxmail.com>
2020-10-09 09:38:42 +0800

    feat (L003 - UserData) : Update the UserData class to include an instance of the user profile that persists even after the user dismisses the profile view.

--- L003-Landmarks/Landmarks/Landmarks/Models/UserData.swift
+++ L003-Landmarks/Landmarks/Landmarks/Models/UserData.swift
@@ -14,5 +14,6 @@ final class UserData: ObservableObject {
 
     @Published var showFavoritesOnly = false
     @Published var landmarks = landmarkData
+    @Published var profile = Profile.default
 
 }
