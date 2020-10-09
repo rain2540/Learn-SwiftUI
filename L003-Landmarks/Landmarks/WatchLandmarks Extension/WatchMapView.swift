84deecfa4df84ba0aa2708afa114ad4e231c7087
Smartech <rain2540@foxmail.com>
2020-10-09 15:36:39 +0800

    feat (L003 - WatchMapView) : Set the map’s region based on the landmark’s coordinates by implementing the WKInterfaceObjectRepresentable protocol’s updateWKInterfaceObject(_:, context:) method.
     - The project now builds without errors.

--- L003-Landmarks/Landmarks/WatchLandmarks Extension/WatchMapView.swift
+++ L003-Landmarks/Landmarks/WatchLandmarks Extension/WatchMapView.swift
@@ -17,6 +17,17 @@ struct WatchMapView: WKInterfaceObjectRepresentable {
         return WKInterfaceMap()
     }
 
+    func updateWKInterfaceObject(_ wkInterfaceObject: WKInterfaceMap, context: WKInterfaceObjectRepresentableContext<WatchMapView>) {
+        let span = MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
+
+        let region = MKCoordinateRegion(
+            center: landmark.locationCoordinate,
+            span: span
+        )
+
+        wkInterfaceObject.setRegion(region)
+    }
+
 }
 
 struct WatchMapView_Previews: PreviewProvider {
