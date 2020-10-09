a9c24e686f2a44ba7922b58bf8dc48cf87b68c43
Smartech <rain2540@foxmail.com>
2020-10-09 11:43:44 +0800

    feat (L003 - PageView) : Declare the @State variable in PageView, and pass a binding to the property when creating the child PageViewController.
     - Important
     - Remember to use the $ syntax to create a binding to a value that is stored as state.

--- L003-Landmarks/Landmarks/Landmarks/PageView.swift
+++ L003-Landmarks/Landmarks/Landmarks/PageView.swift
@@ -11,13 +11,14 @@ import SwiftUI
 struct PageView<Page: View>: View {
 
     var viewControllers: [UIHostingController<Page>]
+    @State var currentPage = 0
 
     init(_ views: [Page]) {
         self.viewControllers = views.map { UIHostingController(rootView: $0) }
     }
 
     var body: some View {
-        PageViewController(controllers: viewControllers)
+        PageViewController(controllers: viewControllers, currentPage: $currentPage)
     }
 }
 
