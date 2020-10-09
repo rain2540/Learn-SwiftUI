d121ee288628a40aadd13cc55479782c458c4d07
Smartech <rain2540@foxmail.com>
2020-10-09 11:19:54 +0800

    feat (L003 - PageView) : Update the preview provider to pass the required array of views, and the preview starts working.

--- L003-Landmarks/Landmarks/Landmarks/PageView.swift
+++ L003-Landmarks/Landmarks/Landmarks/PageView.swift
@@ -23,6 +23,7 @@ struct PageView<Page: View>: View {
 
 struct PageView_Previews: PreviewProvider {
     static var previews: some View {
-        PageView()
+        PageView(features.map { FeatureCard(landmark: $0) })
+            .aspectRatio(3 / 2, contentMode: .fit)
     }
 }
