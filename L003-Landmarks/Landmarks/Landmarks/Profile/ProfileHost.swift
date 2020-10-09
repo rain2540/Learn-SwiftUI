26f71b98fc4ae575b1f52b39d8e9064e739462f6
Smartech <rain2540@foxmail.com>
2020-10-09 09:40:47 +0800

    feat (L003 - ProfileHost) : Read the user’s profile data from the environment to pass control of the data to the profile host.
     - To avoid updating the global app state before confirming any edits — such as while the user enters their name — the editing view operates on a copy of itself.

--- L003-Landmarks/Landmarks/Landmarks/Profile/ProfileHost.swift
+++ L003-Landmarks/Landmarks/Landmarks/Profile/ProfileHost.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct ProfileHost: View {
 
     @Environment(\.editMode) var mode
+    @EnvironmentObject var userData: UserData
     @State var draftProfile = Profile.default
 
     var body: some View {
@@ -29,6 +30,6 @@ struct ProfileHost: View {
 
 struct ProfileHost_Previews: PreviewProvider {
     static var previews: some View {
-        ProfileHost()
+        ProfileHost().environmentObject(UserData())
     }
 }
