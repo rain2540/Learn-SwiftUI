1d6ba2c3a1356c8cdc56f245cbce66c5c7e8d501
Smartech <rain2540@foxmail.com>
2020-10-09 14:46:09 +0800

    feat (L003 - WatchLandmarkDetail) : In WatchLandmarkDetail.swift, return a CircleImage view from the body() method.
     - Here you reuse the CircleImage view from the iOS project. Because you created a resizable image, the call to .scaledToFill() adjusts the circle’s size so that it fills the display.

--- L003-Landmarks/Landmarks/WatchLandmarks Extension/WatchLandmarkDetail.swift
+++ L003-Landmarks/Landmarks/WatchLandmarks Extension/WatchLandmarkDetail.swift
@@ -9,7 +9,7 @@
 import SwiftUI
 
 struct WatchLandmarkDetail: View {
-
+    
     @EnvironmentObject var userData: UserData
     var landmark: Landmark
 
@@ -18,7 +18,8 @@ struct WatchLandmarkDetail: View {
     }
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        CircleImage(image: self.landmark.image.resizable())
+            .scaledToFill()
     }
 }
 
