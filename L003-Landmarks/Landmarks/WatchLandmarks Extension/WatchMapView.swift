cc5abeaa9d6f558013bf44fe5111075fd0ddc6b0
Smartech <rain2540@foxmail.com>
2020-10-09 15:26:17 +0800

    chore (L003 - project) : Add a custom view to the WatchKit extension, named WatchMapView.

---
+++ L003-Landmarks/Landmarks/WatchLandmarks Extension/WatchMapView.swift
@@ -0,0 +1,21 @@
+//
+//  WatchMapView.swift
+//  WatchLandmarks Extension
+//
+//  Created by RAIN on 2020/10/9.
+//  Copyright © 2020 Smartech. All rights reserved.
+//
+
+import SwiftUI
+
+struct WatchMapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WatchMapView_Previews: PreviewProvider {
+    static var previews: some View {
+        WatchMapView()
+    }
+}
