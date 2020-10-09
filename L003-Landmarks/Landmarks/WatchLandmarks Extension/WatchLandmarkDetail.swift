109689df7a8ba05f20eb49e6082f0defb3311d18
Smartech <rain2540@foxmail.com>
2020-10-09 14:35:55 +0800

    chore (L003 - project) : In the Project navigator, click the disclosure triangle next to the WatchLandmarks Extension folder to reveal its contents, and add a new SwiftUI view named WatchLandmarkDetail.

---
+++ L003-Landmarks/Landmarks/WatchLandmarks Extension/WatchLandmarkDetail.swift
@@ -0,0 +1,21 @@
+//
+//  WatchLandmarkDetail.swift
+//  WatchLandmarks Extension
+//
+//  Created by RAIN on 2020/10/9.
+//  Copyright © 2020 Smartech. All rights reserved.
+//
+
+import SwiftUI
+
+struct WatchLandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WatchLandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        WatchLandmarkDetail()
+    }
+}
