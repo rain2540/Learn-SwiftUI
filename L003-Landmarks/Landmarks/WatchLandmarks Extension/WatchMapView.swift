b935a9ac17054c78dd8b5912e31af0ec06c4180a
Smartech <rain2540@foxmail.com>
2020-10-09 15:27:37 +0800

    feat (L003 - WatchMapView) : In the WatchMapView structure, change View to WKInterfaceObjectRepresentable.

--- L003-Landmarks/Landmarks/WatchLandmarks Extension/WatchMapView.swift
+++ L003-Landmarks/Landmarks/WatchLandmarks Extension/WatchMapView.swift
@@ -8,7 +8,7 @@
 
 import SwiftUI
 
-struct WatchMapView: View {
+struct WatchMapView: WKInterfaceObjectRepresentable {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
