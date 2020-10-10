139bde50e96c479322df788783fe975a53f6a57d
Smartech <rain2540@foxmail.com>
2020-10-10 11:36:06 +0800

    feat (L003 - NavigationPrimary) : Declare the filter’s state.
     - Adding state here makes this view the source of truth for that information. You’ll bind this property to both the filter and list views in the next few steps.

--- L003-Landmarks/Landmarks/MacLandmarks/NavigationPrimary.swift
+++ L003-Landmarks/Landmarks/MacLandmarks/NavigationPrimary.swift
@@ -9,9 +9,13 @@
 import SwiftUI
 
 struct NavigationPrimary: View {
+
+    @State private var filter: FilterType = .all
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
+
 }
 
 struct NavigationPrimary_Previews: PreviewProvider {
