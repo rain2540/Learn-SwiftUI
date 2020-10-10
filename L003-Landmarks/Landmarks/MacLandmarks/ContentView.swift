c11be77acf0f5ea66415d060c8ecf2507eaffda0
Smartech <rain2540@foxmail.com>
2020-10-10 09:05:15 +0800

    chore (L003 - project) : files for macOS App

---
+++ L003-Landmarks/Landmarks/MacLandmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MacLandmarks
+//
+//  Created by RAIN on 2020/10/10.
+//  Copyright © 2020 Smartech. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
