b8fe0b7099c98c2b3df5cc5ae00ed6ce8df65436
Smartech <rain2540@foxmail.com>
2020-10-10 10:23:44 +0800

    chore (L003 - project) : Add a new SwiftUI view to the build called Filter.swift.

---
+++ L003-Landmarks/Landmarks/MacLandmarks/Filter.swift
@@ -0,0 +1,23 @@
+//
+//  Filter.swift
+//  MacLandmarks
+//
+//  Created by RAIN on 2020/10/10.
+//  Copyright © 2020 Smartech. All rights reserved.
+//
+
+import SwiftUI
+
+struct Filter: View {
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+    
+}
+
+struct Filter_Previews: PreviewProvider {
+    static var previews: some View {
+        Filter()
+    }
+}
