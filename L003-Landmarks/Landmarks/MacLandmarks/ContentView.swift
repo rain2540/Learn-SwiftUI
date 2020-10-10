be624df87ae2faa19821c2771bc4c9bf6544e44b
Smartech <rain2540@foxmail.com>
2020-10-10 14:06:12 +0800

    feat (L003 - ContentView) : Add a property for the selected landmark and mark it with the @State attribute.
     - By using an optional value for the selected landmark, you avoid having to set a default value. This means that both the preview and the initial state of the app will render without a selected landmark.

--- L003-Landmarks/Landmarks/MacLandmarks/ContentView.swift
+++ L003-Landmarks/Landmarks/MacLandmarks/ContentView.swift
@@ -9,10 +9,14 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var selectedLandmark: Landmark?
+
     var body: some View {
         Text("Hello, World!")
             .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
+
 }
 
 
