54c05eebb1e122c7e0e858b735784334a2a27baf
Smartech <rain2540@foxmail.com>
2020-10-10 09:30:13 +0800

    feat (L003 - LandmarkRow) : Add a landmark property to to the LandmarkRow structure, and update the preview provider with a landmark to display.

--- L003-Landmarks/Landmarks/MacLandmarks/LandmarkRow.swift
+++ L003-Landmarks/Landmarks/MacLandmarks/LandmarkRow.swift
@@ -9,13 +9,17 @@
 import SwiftUI
 
 struct LandmarkRow: View {
+
+    var landmark: Landmark
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
+
 }
 
 struct LandmarkRow_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkRow()
+        LandmarkRow(landmark: landmarkData[0])
     }
 }
