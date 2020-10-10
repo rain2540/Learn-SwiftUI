ead19dfbd72ecb8f4df919962f0b014570b3b320
Smartech <rain2540@foxmail.com>
2020-10-10 11:34:34 +0800

    chore (L003 - project) : Add a new SwiftUI view, named NavigationPrimary.swift, to the project.

---
+++ L003-Landmarks/Landmarks/MacLandmarks/NavigationPrimary.swift
@@ -0,0 +1,21 @@
+//
+//  NavigationPrimary.swift
+//  MacLandmarks
+//
+//  Created by RAIN on 2020/10/10.
+//  Copyright © 2020 Smartech. All rights reserved.
+//
+
+import SwiftUI
+
+struct NavigationPrimary: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NavigationPrimary_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationPrimary()
+    }
+}
