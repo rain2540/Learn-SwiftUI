25693fdbe717363c3ff278a00b7af0c1923e2030
Smartech <rain2540@foxmail.com>
2020-10-10 14:21:19 +0800

    refactor (L003 - MapView) : update span

--- L003-Landmarks/Landmarks/Landmarks/Views/MapView.swift
+++ L003-Landmarks/Landmarks/Landmarks/Views/MapView.swift
@@ -18,8 +18,7 @@ struct MapView {
     }
 
     func updateMapView(_ view: MKMapView, context: Context) {
-        let coordinate = CLLocationCoordinate2D(latitude: 34.011286, longitude: -116.166868)
-        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let span = MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
         let region = MKCoordinateRegion(center: coordinate, span: span)
         view.setRegion(region, animated: true)
     }
