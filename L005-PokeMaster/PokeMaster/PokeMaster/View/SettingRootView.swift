82ee8aabb61df5f2b8dfd5fbd71ef24728c587ae
Smartech <rain2540@foxmail.com>
2020-11-27 17:06:24 +0800

    feat (L005 - project) : add SettingRootView

---
+++ L005-PokeMaster/PokeMaster/PokeMaster/View/SettingRootView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingRootView.swift
+//  PokeMaster
+//
+//  Created by RAIN on 2020/11/27.
+//
+
+import SwiftUI
+
+struct SettingRootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingRootView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingRootView()
+    }
+}
