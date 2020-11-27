5bdd7b36adbaa54eeba24f9b258a1f57dcab8b5c
Smartech <rain2540@foxmail.com>
2020-11-27 16:15:35 +0800

    chore (L005 - project) : add PokemonInfoPanelHeader

---
+++ L005-PokeMaster/PokeMaster/PokeMaster/View/PokemonInfoPanelHeader.swift
@@ -0,0 +1,20 @@
+//
+//  PokemonInfoPanelHeader.swift
+//  PokeMaster
+//
+//  Created by RAIN on 2020/11/27.
+//
+
+import SwiftUI
+
+struct PokemonInfoPanelHeader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PokemonInfoPanelHeader_Previews: PreviewProvider {
+    static var previews: some View {
+        PokemonInfoPanelHeader()
+    }
+}
