b4fdddf25bb340c2d8cc2bcfe04bd463af37c12a
Smartech <rain2540@foxmail.com>
2020-11-27 16:29:42 +0800

    feat (L005 - Settings) : add sub type

--- L005-PokeMaster/PokeMaster/PokeMaster/View/SettingView.swift
+++ L005-PokeMaster/PokeMaster/PokeMaster/View/SettingView.swift
@@ -7,6 +7,20 @@
 
 import SwiftUI
 
+
+class Settings: ObservableObject {
+
+    enum AccountBehavior: CaseIterable {
+        case register, login
+    }
+
+    enum Sorting: CaseIterable {
+        case id, name, color, favorite
+    }
+
+}
+
+
 struct SettingView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
