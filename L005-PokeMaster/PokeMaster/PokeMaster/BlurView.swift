fb364a1d6aafd07af966bef925679b621d962158
Smartech <rain2540@foxmail.com>
2020-11-27 16:05:51 +0800

    chore (L005 - project) : add BlurView

---
+++ L005-PokeMaster/PokeMaster/PokeMaster/BlurView.swift
@@ -0,0 +1,20 @@
+//
+//  BlurView.swift
+//  PokeMaster
+//
+//  Created by RAIN on 2020/11/27.
+//
+
+import SwiftUI
+
+struct BlurView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BlurView_Previews: PreviewProvider {
+    static var previews: some View {
+        BlurView()
+    }
+}
