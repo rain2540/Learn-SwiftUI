d6f0fc7843132126bd429603b6e77a79065fb827
Smartech <rain2540@foxmail.com>
2020-11-27 15:28:19 +0800

    feat (L005 - PokemonInfoPanelAbilityList) : add subviews and layout them

--- L005-PokeMaster/PokeMaster/PokeMaster/PokemonInfoPanelAbilityList.swift
+++ L005-PokeMaster/PokeMaster/PokeMaster/PokemonInfoPanelAbilityList.swift
@@ -7,14 +7,42 @@
 
 import SwiftUI
 
-struct PokemonInfoPanelAbilityList: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+extension PokemonInfoPanel {
+
+    struct AbilityList: View {
+
+        let model: PokemonViewModel
+        let abilityModels: [AbilityViewModel]?
+        
+        var body: some View {
+            VStack(alignment: .leading, spacing: 12, content: {
+                Text("技能")
+                    .font(.headline)
+                    .fontWeight(.bold)
+                if abilityModels != nil {
+                    ForEach(abilityModels!) { ability in
+                        Text(ability.name)
+                            .font(.subheadline)
+                            .foregroundColor(self.model.color)
+                        Text(ability.descriptionText)
+                            .font(.footnote)
+                            .foregroundColor(Color(hex: 0xAAAAAA))
+                            .fixedSize(horizontal: false, vertical: true)
+                    }
+                }
+            })
+            .frame(maxWidth: .infinity, alignment: .leading)
+        }
+
     }
+
 }
 
+
+/*
 struct PokemonInfoPanelAbilityList_Previews: PreviewProvider {
     static var previews: some View {
         PokemonInfoPanelAbilityList()
     }
 }
+*/
