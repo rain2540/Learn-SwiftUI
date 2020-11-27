4901bb4a095db2b9b67750b21df4ed534c254bcd
Smartech <rain2540@foxmail.com>
2020-11-27 14:35:14 +0800

    chore (L005 - project) : add PokemonInfoPanelAbilityList

---
+++ L005-PokeMaster/PokeMaster/PokeMaster/PokemonInfoPanelAbilityList.swift
@@ -0,0 +1,20 @@
+//
+//  PokemonInfoPanelAbilityList.swift
+//  PokeMaster
+//
+//  Created by RAIN on 2020/11/27.
+//
+
+import SwiftUI
+
+struct PokemonInfoPanelAbilityList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PokemonInfoPanelAbilityList_Previews: PreviewProvider {
+    static var previews: some View {
+        PokemonInfoPanelAbilityList()
+    }
+}
