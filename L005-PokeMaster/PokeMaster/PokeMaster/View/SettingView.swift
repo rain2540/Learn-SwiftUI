db192e430285f7e4913a4819736299221c63587b
Smartech <rain2540@foxmail.com>
2020-11-27 16:44:41 +0800

    feat (L005 - SettingView) : add Form

--- L005-PokeMaster/PokeMaster/PokeMaster/View/SettingView.swift
+++ L005-PokeMaster/PokeMaster/PokeMaster/View/SettingView.swift
@@ -61,7 +61,9 @@ struct SettingView: View {
     @ObservedObject var settings = Settings()
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            
+        }
     }
 
 }
