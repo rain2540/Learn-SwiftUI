7e40fdfe826e180305f24167d8af1ed51d4c0029
Smartech <rain2540@foxmail.com>
2020-11-30 14:48:41 +0800

    feat (L005 - project) : add PokemonRootView

---
+++ L005-PokeMaster/PokeMaster/PokeMaster/View/List/PokemonRootView.swift
@@ -0,0 +1,20 @@
+//
+//  PokemonRootView.swift
+//  PokeMaster
+//
+//  Created by RAIN on 2020/11/30.
+//
+
+import SwiftUI
+
+struct PokemonRootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PokemonRootView_Previews: PreviewProvider {
+    static var previews: some View {
+        PokemonRootView()
+    }
+}
