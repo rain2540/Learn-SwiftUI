0353df2bf1e75f62f55ba2a6a7d0dbc7b9aee905
Smartech <rain2540@foxmail.com>
2021-01-12 08:16:50 +0800

    chore (L005 - project) : add MainTab

---
+++ L005-PokeMaster/PokeMaster/PokeMaster/View/MainTab.swift
@@ -0,0 +1,20 @@
+//
+//  MainTab.swift
+//  PokeMaster
+//
+//  Created by RAIN on 2021/1/12.
+//
+
+import SwiftUI
+
+struct MainTab: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainTab_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTab()
+    }
+}
