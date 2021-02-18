0e2859255eae885baa42e8d620b05f104209b372
Smartech <rain2540@foxmail.com>
2021-02-18 15:42:05 +0800

    feat (L005 - Store) : add a static method

--- L005-PokeMaster/PokeMaster/PokeMaster/Store.swift
+++ L005-PokeMaster/PokeMaster/PokeMaster/Store.swift
@@ -12,4 +12,20 @@ class Store: ObservableObject {
 
     @Published var appState = AppState()
 
+    static func reduce(
+        state: AppState,
+        action: AppAction) -> AppState
+    {
+        var appState = state
+
+        switch action {
+            case .login(let email, let password):
+                if password == "password" {
+                    let user = User(email: email, favoritePokemonIDs: [])
+                    appState.settings.loginUser = user
+                }
+        }
+        return appState
+    }
+
 }
