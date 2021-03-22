3c31d5cd78ae00db03ea8b78109da5d66592aa32
Smartech <rain_2540@foxmail.com>
2021-03-22 17:12:14 +0800

    feat (L005 - LoginRequest) : add properties

--- L005-PokeMaster/PokeMaster/PokeMaster/LoginRequest.swift
+++ L005-PokeMaster/PokeMaster/PokeMaster/LoginRequest.swift
@@ -6,7 +6,26 @@
 //
 
 import Foundation
+import Combine
 
 struct LoginRequest {
+
+    let email: String
+    let password: String
+
+    var publisher: AnyPublisher<User, AppError> {
+        Future { promise in
+            DispatchQueue.global().asyncAfter(deadline: .now() + 1.5) {
+                if self.password == "password" {
+                    let user = User(email: self.email, favoritePokemonIDs: [])
+                    promise(.success(user))
+                } else {
+                    promise(.failure(.passwordWrong))
+                }
+            }
+        }
+        .receive(on: DispatchQueue.main)
+        .eraseToAnyPublisher()
+    }
 
 }
