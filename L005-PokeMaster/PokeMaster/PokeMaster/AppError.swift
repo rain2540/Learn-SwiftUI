2a83a5681cb87d4cec5d0d3f8f7a64ccd6902782
Smartech <rain_2540@foxmail.com>
2021-03-22 17:11:47 +0800

    feat (L005 - AppError) : add case and property

--- L005-PokeMaster/PokeMaster/PokeMaster/AppError.swift
+++ L005-PokeMaster/PokeMaster/PokeMaster/AppError.swift
@@ -7,6 +7,22 @@
 
 import Foundation
 
-enum AppError: Error {
+enum AppError: Error, Identifiable {
+
+    var id: String { localizedDescription }
+
+    case passwordWrong
+
+}
+
+
+extension AppError: LocalizedError {
+
+    var localizedDescription: String {
+        switch self {
+            case .passwordWrong:
+                return "密码错误"
+        }
+    }
 
 }
