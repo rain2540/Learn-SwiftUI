08f37dcf8c1e5a86c54f1454b44f89d15a0b02c3
Smartech <rain_2540@foxmail.com>
2021-05-06 08:25:07 +0800

    feat (L005 - FileStorage) : wrappedValue

--- L005-PokeMaster/PokeMaster/PokeMaster/State/FileStorage.swift
+++ L005-PokeMaster/PokeMaster/PokeMaster/State/FileStorage.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-
+@propertyWrapper
 struct FileStorage<T: Codable> {
 
     var value: T?
@@ -23,6 +23,26 @@ struct FileStorage<T: Codable> {
         )
         self.directory = directory
         self.fileName = fileName
+    }
+
+    var wrappedValue: T? {
+        set {
+            value = newValue
+            if let value = newValue {
+                try? FileHelper.writeJSON(
+                    value,
+                    to: directory,
+                    fileName: fileName
+                )
+            } else {
+                try? FileHelper.delete(
+                    from: directory,
+                    fileName: fileName
+                )
+            }
+        }
+
+        get { value }
     }
 
 }
