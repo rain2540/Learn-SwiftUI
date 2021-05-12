599eeefd0911eea9d6a2f9419e46014572014374
Smartech <rain_2540@foxmail.com>
2021-05-12 16:57:41 +0800

    feat (L005 - EmailCheckingRequest) : add properties

--- L005-PokeMaster/PokeMaster/PokeMaster/State/Networking/EmailCheckingRequest.swift
+++ L005-PokeMaster/PokeMaster/PokeMaster/State/Networking/EmailCheckingRequest.swift
@@ -6,7 +6,25 @@
 //
 
 import Foundation
+import Combine
 
 struct EmailCheckingRequest {
-    
+
+    let email: String
+
+    var publisher: AnyPublisher<Bool, Never> {
+        Future<Bool, Never> { promise in
+            DispatchQueue.global().asyncAfter(deadline: .now() + 0.5) {
+                if self.email.lowercased() == "onevcat@gmail.com" {
+                    promise(.success(false))
+                } else {
+                    promise(.success(true))
+                }
+            }
+        }
+        .receive(on: DispatchQueue.main)
+        .eraseToAnyPublisher()
+    }
+
+
 }
