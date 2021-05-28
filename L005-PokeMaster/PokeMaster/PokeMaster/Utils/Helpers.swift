40601728ceace030fbd7f03d03d0148bebef933d
Smartech <rain2540@foxmail.com>
2021-05-28 09:25:56 +0800

    feat (L005 - Helpers) : add a helper
     - zip all for Publisher Array

--- L005-PokeMaster/PokeMaster/PokeMaster/Utils/Helpers.swift
+++ L005-PokeMaster/PokeMaster/PokeMaster/Utils/Helpers.swift
@@ -56,3 +56,19 @@ let appEncoder: JSONEncoder = {
     encoder.keyEncodingStrategy = .convertToSnakeCase
     return encoder
 }()
+
+
+import Combine
+
+extension Array where Element: Publisher {
+
+    var zipAll: AnyPublisher<[Element.Output], Element.Failure> {
+        let initial = Just([Element.Output]())
+            .setFailureType(to: Element.Failure.self)
+            .eraseToAnyPublisher()
+        return reduce(initial) { result, publisher in
+            result.zip(publisher) { $0 + [$1] }.eraseToAnyPublisher()
+        }
+    }
+
+}
