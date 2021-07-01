aae313e35c2ababb83e752c468622419278ae7d4
Rain2540 <rain2540@foxmail.com>
2021-07-01 21:47:21 +0800

    chore (L006 - Files) : add files

---
+++ L006-Memorize/Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Rain on 2021/7/1.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
