af88df37ac1a3d027ea922801b89a8f700b131f1
Rain2540 <rain2540@foxmail.com>
2021-07-01 22:01:33 +0800

    feat (L006 - ContentView) : ZStack

--- L006-Memorize/Memorize/Memorize/ContentView.swift
+++ L006-Memorize/Memorize/Memorize/ContentView.swift
@@ -9,12 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        RoundedRectangle(cornerRadius: 20)
-            .stroke(lineWidth: 3)
-            .padding(.horizontal)
-            .foregroundColor(.red)
-        //Text("Hello, world!").foregroundColor(/*@START_MENU_TOKEN@*/.orange/*@END_MENU_TOKEN@*/).padding()
-
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(lineWidth: 3)
+                .padding(.horizontal)
+                .foregroundColor(.red)
+            Text("Hello, world!")
+                .foregroundColor(/*@START_MENU_TOKEN@*/.orange/*@END_MENU_TOKEN@*/)
+                .padding()
+        }
     }
 }
 
