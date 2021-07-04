83ddfb6cbc781363c64e11a2a97881419decd167
Rain2540 <rain2540@foxmail.com>
2021-07-04 22:59:29 +0800

    feat (L006 - ContentView) : preview for dark mode and light mode

--- L006-Memorize/Memorize/Memorize/ContentView.swift
+++ L006-Memorize/Memorize/Memorize/ContentView.swift
@@ -39,5 +39,8 @@ struct CardView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
     }
 }
