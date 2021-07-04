d50e05cbc84f98edd52a927370ba5f669ba894fa
Rain2540 <rain2540@foxmail.com>
2021-07-04 23:03:40 +0800

    feat (L006 - CardView) : fill with white color

--- L006-Memorize/Memorize/Memorize/ContentView.swift
+++ L006-Memorize/Memorize/Memorize/ContentView.swift
@@ -28,6 +28,9 @@ struct CardView: View {
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 20)
+                .fill()
+                .foregroundColor(.white)
+            RoundedRectangle(cornerRadius: 20)
                 .stroke(lineWidth: 3)
             Text("✈️")
                 .font(.largeTitle)
