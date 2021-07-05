b3a4cf4cbee73273ed523eef805b437d0f4adfd4
Rain2540 <rain2540@foxmail.com>
2021-07-05 22:31:43 +0800

    feat (L006 - CardView) : variable 'shape' uses at more than one place

--- L006-Memorize/Memorize/Memorize/ContentView.swift
+++ L006-Memorize/Memorize/Memorize/ContentView.swift
@@ -29,17 +29,13 @@ struct CardView: View {
 
     var body: some View {
         ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 20)
-                    .fill()
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 20)
-                    .stroke(lineWidth: 3)
-                Text("✈️")
-                    .font(.largeTitle)
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+                Text("✈️").font(.largeTitle)
             } else {
-                RoundedRectangle(cornerRadius: 20)
-                    .fill()
+                shape.fill()
             }
         }
     }
