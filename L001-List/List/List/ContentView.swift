20155812d51b8fa031a4f78a06f6df4d887c5fbd
Rain2540 <rain2540@foxmail.com>
2022-07-03 22:18:24 +0800

    style (ContentView) : update style

--- L001-List/List/List/ContentView.swift
+++ L001-List/List/List/ContentView.swift
@@ -8,31 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    var body: some View {
-        TabView {
-            ListExampleView()
-                .tabItem {
-                    Image(systemName: "circle")
-                    Text("List")
-                }
-            FormExampleView()
-                .tabItem {
-                    tabbarItem(text: "Form", image: "icon-movies")
-                }
+
+  var body: some View {
+    TabView {
+      ListExampleView()
+        .tabItem {
+          Image(systemName: "circle")
+          Text("List")
+        }
+      FormExampleView()
+        .tabItem {
+          tabbarItem(text: "Form", image: "icon-movies")
         }
     }
-    
-    func tabbarItem(text: String, image: String) -> some View {
-        VStack {
-            Image(image)
-            Text(text)
-        }
+  }
+
+  func tabbarItem(text: String, image: String) -> some View {
+    VStack {
+      Image(image)
+      Text(text)
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
